6b379b616b53fd43b7118fdf30ad2f67a6a917be
Francesco Diotallevi <78257415+OtiDioti@users.noreply.github.com>
2024-11-19 09:14:45 +0100

    changed name in setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="PySW",            # Package name
+    name="SymPT",            # Package name
     version="0.9.9",              # Package version
     description="A Python package with classes, utils, and solver functionality",  # Short description
     long_description=open("README.md").read(),  # Detailed description from README
     long_description_content_type="text/markdown",  # Specify markdown if README.md is used
     author="- Giovanni Francesco Diotallevi - Irving Leander Reascos Valencia",           # Author's name
     author_email="francesco.diotallevi@uni-a.de; irving.reascos.valencia@uni-a.de",  # Author's email
-    url="https://github.com/qcode-uni-a/PySW",  # Package URL (if available)
+    url="https://github.com/qcode-uni-a/SymPT",  # Package URL (if available)
     packages=find_packages(),     # Automatically find packages in your project
     install_requires=[
         "ipython",
